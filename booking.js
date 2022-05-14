55b475c95d075827c0064ab8a0db46389baeb76b
Kealen-Pillay <kealenpillay@gmail.com>
2022-05-14 16:54:46 +1200

    Minor changes

--- booking.js
+++ booking.js
@@ -16,20 +16,24 @@ function addHandler() {
 }
 
 function validateFields() {
-  var customerName = document.getElementById("cname");
-  var phoneNumber = document.getElementById("phone");
-  var streetNumber = document.getElementById("stnumnber");
-  var streetName = document.getElementById("sname");
-  var pickupDate = document.getElementById("date");
-  var pickupTime = document.getElementById("time");
+  var customerName = document.getElementById("cname").value;
+  var phoneNumber = document.getElementById("phone").value;
+  var streetNumber = document.getElementById("stnumnber").value;
+  var streetName = document.getElementById("sname").value;
+  var pickupDate = document.getElementById("date").value;
+  var pickupTime = document.getElementById("time").value;
 
-  if (
-    customerName == null ||
-    phoneNumber == null ||
-    streetNumber == null ||
-    streetName == null ||
-    pickupDate == null ||
-    pickupTime == null
-  ) {
+  if (customerName == null) {
+    alert("Please provide the customer name!");
+  } else if (phoneNumber == null) {
+    alert("Please provide the phone number!");
+  } else if (streetNumber == null) {
+    alert("please provide the street number!");
+  } else if (streetName == null) {
+    alert("please provide the street name!");
+  } else if (pickupDate == null) {
+    alert("Please provide a pickup date!");
+  } else if (pickupTime == null) {
+    alert("Please provide a pickup time!");
   }
 }
