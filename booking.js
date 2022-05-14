8698f3f8d6182d3b3ad8645f554ff395b4fb5415
Kealen-Pillay <kealenpillay@gmail.com>
2022-05-14 15:11:27 +1200

    Added default date and time to homepage

--- booking.js
+++ booking.js
@@ -3,7 +3,10 @@ function setDate() {
   var year = date.getFullYear();
   var month = String(date.getMonth()+1).padStart(2,'0');
   var day = String(date.getDate()).padStart(2, "0");
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var timePattern = hours + ":" + minutes;
   var datePattern = year + '-' + month + '-' + day;
   document.getElementById("date").value = datePattern;
+  document.getElementById("time").value=timePattern;
 }
-
