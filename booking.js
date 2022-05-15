15d4cfdb3879373dd8b5c84bf4883fca998d1f88
Kealen-Pillay <kealenpillay@gmail.com>
2022-05-15 18:59:59 +1200

    Handling client side

--- booking.js
+++ booking.js
@@ -15,26 +15,31 @@ function addHandler() {
   document.getElementById("submit").addEventListener("click", false);
 }
 
-function validateFields() {
-  var customerName = document.getElementById("cname").value;
-  var phoneNumber = document.getElementById("phone").value;
-  var streetNumber = document.getElementById("snumber").value;
-  var streetName = document.getElementById("stname").value;
-  var pickupDate = document.getElementById("date").value;
-  var pickupTime = document.getElementById("time").value;
+function validateFields(
+  cname,
+  phone,
+  unumber,
+  snumber,
+  stname,
+  sbname,
+  dsbname,
+  date,
+  time,
+  targetDiv
+) {
   var notNull = false;
 
-  if (customerName === "") {
+  if (cname === "") {
     alert("Please provide the customer name!");
-  } else if (phoneNumber === "") {
+  } else if (phone === "") {
     alert("Please provide the phone number!");
-  } else if (streetNumber === "") {
+  } else if (snumber === "") {
     alert("please provide the street number!");
-  } else if (streetName === "") {
+  } else if (stname === "") {
     alert("please provide the street name!");
-  } else if (pickupDate == null) {
+  } else if (date == null) {
     alert("Please provide a pickup date!");
-  } else if (pickupTime === "") {
+  } else if (time === "") {
     alert("Please provide a pickup time!");
   } else {
     notNull = true;
@@ -43,6 +48,73 @@ function validateFields() {
   if (notNull) {
     if (phoneNumber.length < 10 || phoneNumber.length > 12) {
       alert("Phone Number Must Be Between 10 - 12 Characters Long!");
+    } else {
+      getData(
+        cname,
+        phone,
+        unumber,
+        snumber,
+        stname,
+        sbname,
+        dsbname,
+        date,
+        time,
+        targetDiv
+      );
     }
+  }
+}
+
+function createRequest() {
+  var xhr = false;
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return xhr;
+}
+
+function getData(
+  cname,
+  phone,
+  unumber,
+  snumber,
+  stname,
+  sbname,
+  dsbname,
+  date,
+  time,
+  targetDiv
+) {
+  var xhr = createRequest();
+  if (xhr) {
+    var place = document.getElementById(divID);
+    var vars =
+      "?cname=" +
+      cname +
+      "&phone=" +
+      phone +
+      "&unumber=" +
+      unumber +
+      "&snumber=" +
+      snumber +
+      "&stname=" +
+      stname +
+      "&sbname=" +
+      sbname +
+      "&dsbname=" +
+      dsbname +
+      "&date=" +
+      date +
+      "&time=" +
+      time;
+    xhr.open("POST", "booking.php", true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        place.innerHTML = xhr.responseText;
+      }
+    };
+    xhr.send(vars);
   }
 }
