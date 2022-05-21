ace2c4b6474941555c29ef1ae55d53969d7d3edf
Kealen-Pillay <kealenpillay@gmail.com>
2022-05-21 16:29:23 +1200

    Successful data entry

--- booking.php
+++ booking.php
@@ -14,7 +14,7 @@
     
     $tableCreationQuery = "CREATE TABLE IF NOT EXISTS requests (bookingNo INT(10) AUTO_INCREMENT PRIMARY KEY, customerName VARCHAR(255), phone INTEGER(12), unitNumber INT(10), streetNumber INTEGER(2), streetName VARCHAR(255), suburb VARCHAR(255), destinationSuburb VARCHAR(255), pickupDate DATE, pickupTime TIME, assignmentStatus VARCHAR(255))";
     $result = @mysqli_query($conn, $tableCreationQuery)
-            or die("<p>Unable to make query</p>"
+            or die("<p>Unable to create table</p>"
             . "<p>Error code " . mysqli_errno($conn)
             . ": " . mysqli_error($conn));
 
@@ -28,21 +28,27 @@
     $dsbname = $_POST["dsbname"];
     $date = $_POST["date"];
     $time = $_POST["time"];
+    $assignmentStatus = "unassigned";
 
     $numRowsQuery = "SELECT bookingNo FROM $sql_table";
     $result = @mysqli_query($conn, $numRowsQuery)
-    or die("<p>Unable to make query</p>"
+    or die("<p>Unable to retrieve rows</p>"
     . "<p>Error code " . mysqli_errno($conn)
     . ": " . mysqli_error($conn));
 
-    $bookingRef = @mysqli_num_rows($result)
-    or die("<p>Unable to make query</p>"
-    . "<p>Error code " . mysqli_errno($conn)
-    . ": " . mysqli_error($conn));
+    if(!$result) {
+        $bookingRef = 0;
+    } else {
+        $bookingRef = mysqli_num_rows($result);
+    }
 
     $newBookingRef = $bookingRef + 1;
 
-    $insertBookingQuery = "INSERT INTO $sql_table (bookingNo, customerName, phone, unitNumber, streetNumber, streetName, suburb, destinationSuburb, pickupDate, pickupTime, assignmentStatus) VALUES ('$newBookingRef', '$cname', '$phone', '$unumber', '$snumber', '$stname', '$sbname', '$dsbname', '$date', '$time')";
+    $insertBookingQuery = "INSERT INTO $sql_table (customerName, phone, unitNumber, streetNumber, streetName, suburb, destinationSuburb, pickupDate, pickupTime, assignmentStatus) VALUES ('$cname', '$phone', '$unumber', '$snumber', '$stname', '$sbname', '$dsbname', '$date', '$time', '$assignmentStatus')";
+    $result = @mysqli_query($conn, $insertBookingQuery)
+    or die("<p>Unable to insert data</p>"
+    . "<p>Error code " . mysqli_errno($conn)
+    . ": " . mysqli_error($conn));
 
     echo "<h3>Thankyou for your booking!</h3>" .
         "<p>Booking Reference Number: ". ($newBookingRef)."</p>" .
